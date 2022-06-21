4f04e04878ed946d8fe412ec5a98d1233224cb95
Kimkun113 <nhungnguyen113579@gmail.com>
2022-06-22 02:23:04 +0900

    web_113/sochanle/sochanle.html

---
+++ web_113/sochanle/sochanle.js
@@ -0,0 +1,12 @@
+function kiemtra(){
+    let num1 =+document.getElementById("number").value;
+    
+    if(num1 % 2 == 0){
+        alert("số"+ " " + num1 +" "+ "là số chẵn");
+    }else{
+        alert("số"+ " " + num1 + " "+ "là số lẻ");
+    }
+    // document.getElementById("result").innerHTML=result;
+    
+
+}
